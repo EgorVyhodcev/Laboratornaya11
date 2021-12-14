2d2d97a108e72a91dcfdc6153df03128b678c00e
EgorVyhodcev <wf-game-acc@bk.ru>
2021-12-14 22:22:30 +0300

    Update

--- LB11/pr2.py
+++ LB11/pr2.py
@@ -4,15 +4,15 @@
 import math
 
 
-def circle(radius):
-    circle_square = math.pi * radius ** 2
-    return circle_square
-
-
 def cylinder():
+
+    def circle(r):
+        circle_square = math.pi * r ** 2
+        return circle_square
+
     radius = float(input("Enter the radius of the cylinder: "))
     height = float(input("Enter the height if the cylinder: "))
-    print("Do you want to compute the full cylinder square?")
+    print("Do you want to compute the full cylinder square? - 'yes' or 'no'")
     command = input().lower()
     if command == 'yes':
         print(2 * math.pi * radius * height + 2 * circle(radius))
